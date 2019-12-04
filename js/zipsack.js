1c3e2a280688548ee90c96c10daff6e253a8e0c7
Tim <timjmartino+github2@gmail.com>
2019-12-03 23:29:59 -0800

    initial fabric size calculator added

--- js/zipsack.js
+++ js/zipsack.js
@@ -3,11 +3,11 @@ $('document').ready(function() {
     var version = "v0.1";
     $('.version').html(version);
 
-    $('.length').keyup(function(event) {
-        var val = $(this).val().replace(/[^\d.-]/,"");
-        $(this).val(val);
-        console.log(".length: "+val);
-    })
+    // $('.length').keyup(function(event) {
+    //     var val = $(this).val().replace(/[^\d.-]/,"");
+    //     $(this).val(val);
+    //     console.log(".length: "+val);
+    // })
     
 
 })
