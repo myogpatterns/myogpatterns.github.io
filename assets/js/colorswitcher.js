e279ab2736176279e305c3c077fffa231aca9054
myogpatterns <timjmartino+github2@gmail.com>
2022-07-12 23:26:19 -0700

    layouts, challenge colors, pack fabrics

---
+++ assets/js/colorswitcher.js
@@ -0,0 +1,35 @@
+window.onload=function(){
+    var palette=[
+        "--learnmyog-purple: #3d3846ff;",
+        "--dark-purple: #312744ff;",	
+        "--prussian-blue: #133754ff;",
+        "--bright-navy-blue: #1874ddff;",
+        "--viridian-green: #019aa5ff;",
+        "--sunglow: #ffcc37ff;",
+        "--pearly-purple: #b063aaff;",
+        "--magenta-dye: #d0046fff;"
+    ];
+    var colors=[
+        "#3d3846",
+        "#3d3846",
+        "#312744",
+        "#312744",
+        "#133754",
+        "#133754",
+        "#1874dd",
+        "#1874dd",
+        "#019aa5",
+        "#019aa5",
+        "#ffcc37",
+        "#b063aa",
+        "#d0045f"
+    ];
+    let rn = Math.floor(Math.random()*colors.length);
+    var color1 = colors[rn];
+    var color2 = colors[Math.floor(Math.random()*colors.length)];
+    $(':root').css('--banner', color1); 
+    if (color2 != color1){
+        $(':root').css('--spotlight', color2);
+    }
+    console.log(rn);
+}
