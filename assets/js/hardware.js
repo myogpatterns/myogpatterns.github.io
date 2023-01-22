fbe9eb5a5fae4cbd7579d567b557f210cdb3aa77
myogpatterns <timjmartino+github2@gmail.com>
2023-01-21 23:23:28 -0800

    map & hw added; new nav bar; map merged in

---
+++ assets/js/hardware.js
@@ -0,0 +1,117 @@
+/*
+    Displays products from allproducts.json
+    allproducts.json created with db-capture.py
+*/
+const allProducts = "./allproducts.json";
+const imgdir = "./"
+
+
+// displayProductsfromArray(data);
+const product_list = getProducts(allProducts);
+
+async function getProducts(array) {
+    const response = await fetch(array);
+    const data = await response.json();
+
+    // view number of elements
+    let id_count = Object.keys(data).length;
+    console.log(id_count + " items");
+    displayProducts(data)
+}
+
+function displayProducts(product_list){
+    product_list.sort((a,b) => a.title.localeCompare(b.title));
+    // console.log(product_list.sort((a,b) => a.title.localeCompare(b.title)).slice(0,3))
+    product_list.forEach(product => {
+        let title = truncateString(product.title.toString(),30);
+        if (typeof product.sizes === "string"){
+            productsizes = product.sizes.replace(",",", ");
+        } else if (typeof product.sizes === "object"){
+            productsizes = product.sizes.join(", ");
+        } else productsizes = product.sizes;
+        
+        const cards = document.querySelector('#card-shelf');
+        let card = document.createElement('div');
+        let shop = product.vendor.replaceAll(" ","-");
+        card.className = `card ${product.product_type} ${shop}`; 
+        // card.dataset.vendor = product.vendor.replaceAll(" ","-");
+        card.innerHTML =
+            `<img src="${product.productImg}" loading="lazy" alt="product image">
+              <div class="card-info">
+                  <p class="title">${title}</p>
+                  <p class="option">${productsizes}</p>
+                  <p class="vendor"><a href=${product.url} target=_blank title="product url"><img src=${imgdir}${product.vendorImg} alt="vendor logo"></a></p>
+              </div>
+            </div>`;
+        cards.appendChild(card);
+    });
+}
+
+/*
+    Trim string to length and add ellipses
+*/
+function truncateString(str, num) {
+    if (str.length > num) {
+      return str.slice(0, num) + "...";
+    } else {
+      return str;
+    }
+}
+
+/* 
+    Filter elements
+*/
+var filters = {};
+// group class selectors from buttons
+$('.filters').on('click', '.button', function( event ) {
+    var $button = $( event.currentTarget );
+    // get group key
+    var $buttonGroup = $button.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $button.attr('data-filter');
+    // combine filters
+    var filterValue = concatValues( filters );
+    // console.log( filterValue )
+    // set filter
+    filterElements( filterValue )
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function( event ) {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        var $button = $( event.currentTarget );
+        $button.addClass('is-checked');
+    });
+});
+// flatten object by concatting values
+function concatValues( obj ) {
+    var value = '';
+    for ( var prop in obj ) {
+      value += obj[ prop ];
+    }
+    return value;
+}
+// flatten object by joining values
+function joinString( obj ) {
+    var value = '';
+    var list = [];
+    for ( var prop in obj ) {
+        list.push( obj[prop] )
+    }
+    value = list.join(" ");
+    return value;
+}
+// show/hide elements
+function filterElements ( filterclasses ) {
+    if (filterclasses.length < 1) {
+        $('.card').show()
+    }
+    else {
+        $('.card').hide()
+        const elements = $(filterclasses);
+        $(elements).show();
+    }
+}
+
---
+++ assets/js/map.js
@@ -0,0 +1,78 @@
+/*
+    Displays vendors from CSV on leaflet.js map
+    CSV from Google Sheets. For LearnMYOG.com.
+*/
+
+
+// https://github.com/risan/country-flag-emoji-json
+const CountryFlagEmojiJSON = "https://cdn.jsdelivr.net/npm/country-flag-emoji-json@2.0.0/dist/index.json"
+
+const map = L.map('map').setView([40, -53], 3);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 9,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+const markers = Papa.parse("assets/db/vendormap.csv", {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+        // console.log("Markers loaded:", results.data)
+        getFlagEmoji( CountryFlagEmojiJSON, results.data )
+    }
+});
+
+async function getFlagEmoji( array, data ) {
+    const response = await fetch(array);
+    const countries = await response.json();
+
+    data.forEach(marker => {
+        // console.log(marker.country);
+        try{ 
+            const found = countries.find(country => country.code == marker.country);
+            marker["flag"] = found.emoji;
+        } catch (TypeError) {
+            marker["flag"] = "🏴‍☠️";
+        }
+        // if (marker.friend) {
+        //     marker["flag"] += "⭐️";
+        // }
+    })
+    drawMarkers( data )
+}
+
+function drawMarkers( data ) {
+    data.sort((a,b) => a.country.localeCompare(b.country));
+
+    data.forEach(marker => {
+        let star = (marker.friend) ? "⭐️" : "";
+        L.marker([marker.lat, marker.long], {
+            icon: marker.friend == true ? greenIcon : blueIcon
+        }).addTo(map)
+        .bindPopup(`<a href="${marker.website}" target="_blank">${star + " " + marker.name}</a>`)
+
+        const stores = $('ul#store-shelf');
+        let store = document.createElement('li');
+        store.className = 'no-deco';
+        store.innerHTML = `${marker.flag} <a href="${marker.website}" target="_blank">${marker.name + " " + star}</a>`
+        stores.append(store)
+    });
+}
+
+
+var LeafIcon = L.Icon.extend({
+    options: {
+      iconSize: [25, 41],
+      iconAnchor: [10, 41],
+      popupAnchor: [2, -40]
+    }
+});
+
+var greenIcon = new LeafIcon({ iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png"  }),
+    yellowIcon = new LeafIcon({iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-gold.png"  }),
+    redIcon = new LeafIcon({iconUrl:"https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png"   });
+    blueIcon = new LeafIcon({iconUrl:"https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png" });
+
