336be0db8e77a722a7c1cc72750d6b1ae363e1f8
Andrew Graham <andrew.grahamg@gmail.com>
2023-02-02 21:39:00 -0700

    add mdx zipper article, update old nav

--- public/assets/js/nav.js
+++ public/assets/js/nav.js
@@ -1,23 +1,23 @@
 class Nav extends HTMLElement {
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-        this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <nav id="nav">
             <ul >
-                <li class="links"><a href="index.html">Patterns</a></li>
+                <li class="links"><a href="/">Patterns</a></li>
                 <li class="links"><a href="zerotohero.html">Getting Started</a></li>
-                <li class="links"><a href="fabrics.html">Fabrics</a></li>
+                <li class="links"><a href="fabrics">Fabrics</a></li>
                 <li class="links"><a href="hardware.html">Hardware</a></li>
-                <li class="links"><a href="map.html">MYOG Suppliers</a></li>
+                <li class="links"><a href="map">MYOG Suppliers</a></li>
                 <li class="menu"><a href="javascript:void(0);" class="icon" onclick="navCollapse()">
                     <i class="fa fa-compass fa-2x"></i>
                 </a></li>
             </ul>
         </nav>
         `;
-    }
+  }
 }
-customElements.define('nav-template', Nav);
+customElements.define("nav-template", Nav);
