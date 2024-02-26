33ebe226ae294f78675c438b92510de2b618f976
myogpatterns <timjmartino+github2@gmail.com>
2024-02-25 16:28:59 -0800

    letter a4 pdf export

--- assets/js/exportToPDF bkup.js
+++ assets/js/exportToPDF bkup.js
@@ -11,40 +11,28 @@ function exportToPDFCircle(diameterInput) {
     // Convert diameter from inches to pixels
     let isMetric = getIsMetric();
 
-    const { diameterInInches, contentWidthInches, contentHeightInches, units, format } = isMetric ?
-        { diameterInInches: parseFloat(diameterInput / 2.54).toFixed(2), contentWidthInches: 7, contentHeightInches: 10, units: 'in', format: 'a4' } :
-        { diameterInInches: diameterInput, contentWidthInches: 7.5, contentHeightInches: 10, units: 'in', format: 'letter' };
+    const { diameterInInches } = isMetric ?
+        { diameterInInches: parseFloat(diameterInput / 2.54).toFixed(2) } :
+        { diameterInInches: diameterInput };
     
     const diameterInPixels = diameterInInches * inchesToPixels;
 
-    // Set canvas size using page content and margin size
+    // Set canvas size to fit the circle and labels with 1/2 inch margin
     const marginInches = 1 / 2;
-    const contentWidthPixels = contentWidthInches * inchesToPixels;
-    const contentHeightPixels = contentHeightInches * inchesToPixels;
     const marginInPixels = marginInches * inchesToPixels;
-    const canvasWidth = contentWidthInches * inchesToPixels + 2 * marginInPixels;
-    const canvasHeight = contentHeightInches * inchesToPixels + 2 * marginInPixels;
+    const canvasWidth = diameterInPixels + 2 * marginInPixels;
+    const canvasHeight = diameterInPixels + 2 * marginInPixels;
     const canvas = document.createElement('canvas');
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
 
-    // Draw on the canvas
+    // Draw the circles on the canvas...
     const ctx = canvas.getContext('2d');
+    ctx.beginPath();
     const centerX = canvasWidth / 2;
     const centerY = canvasHeight / 2;
     const outerRadius = diameterInPixels / 2 + (sa * inchesToPixels);
     const innerRadius = diameterInPixels / 2;
-    
-    // Draw the content box
-    ctx.beginPath();
-    ctx.rect(marginInPixels, marginInPixels, contentWidthPixels, contentHeightPixels); //bounding box inside margin
-    ctx.fillStyle = 'none'; // No fill
-    ctx.strokeStyle = '#000'; // Black stroke
-    ctx.lineWidth = 4;
-    ctx.stroke();
-
-    // Draw the circles
-    ctx.beginPath();
     ctx.arc(centerX, centerY, outerRadius, 0, Math.PI * 2);
     ctx.fillStyle = '#fff'; // White fill
     ctx.strokeStyle = '#000'; // Black stroke
@@ -87,13 +75,13 @@ function exportToPDFCircle(diameterInput) {
 
     // Create a new jsPDF instance with adjusted page size
     const pdf = new jsPDF({
-        unit: units,
-        format: format // Adjusted page size
+        unit: 'in',
+        format: [canvasWidth * scale, canvasHeight * scale] // Adjusted page size
     });
 
     // Add the canvas as an image to the PDF with adjusted dimensions
     pdf.addImage(canvas.toDataURL(), 'PNG', 0, 0, canvasWidth * scale, canvasHeight * scale);
 
     // Save PDF
-    pdf.save('Bottom Panel.pdf');
+    pdf.save('circle.pdf');
 }
