300ec82c0d09b65fff60fc7c0701ed1e23b9fedd
myogpatterns <timjmartino+github2@gmail.com>
2024-05-03 20:07:22 -0700

    fixed relative nav links

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -7,11 +7,11 @@ class Nav extends HTMLElement {
         this.innerHTML = `
         <nav id="nav">
             <ul >
-                <li class="links"><a href="index.html">Patterns</a></li>
-                <li class="links"><a href="zerotohero.html">Getting Started</a></li>
-                <li class="links"><a href="fabrics.html">Fabrics</a></li>
-                <li class="links"><a href="hardware.html">Hardware</a></li>
-                <li class="links"><a href="map.html">MYOG Suppliers</a></li>
+                <li class="links"><a href="/index.html">Patterns</a></li>
+                <li class="links"><a href="/zerotohero.html">Getting Started</a></li>
+                <li class="links"><a href="/fabrics.html">Fabrics</a></li>
+                <li class="links"><a href="/hardware.html">Hardware</a></li>
+                <li class="links"><a href="/map.html">MYOG Suppliers</a></li>
                 <li class="menu"><a href="javascript:void(0);" class="icon" onclick="navCollapse()">
                     <i class="fa fa-compass fa-2x"></i>
                 </a></li>
